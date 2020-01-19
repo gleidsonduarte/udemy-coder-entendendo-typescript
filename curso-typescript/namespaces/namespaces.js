fd25bdd3e50bd754125a3cb242e818d5049d25c6
Gleidson Duarte <work.gleidsonduarte@hotmail.com>
2020-01-19 19:09:16 -0300

    Finalizadas as seções 5, 6 e inicializada a 7.

---
+++ curso-typescript/namespaces/namespaces.js
@@ -0,0 +1,5 @@
+"use strict";
+// const PI = 2.99
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
+//# sourceMappingURL=namespaces.js.map
---
+++ curso-typescript/namespaces/namespaces.ts
@@ -0,0 +1,4 @@
+// const PI = 2.99
+
+console.log(Geometria.Area.circunferencia(10))
+console.log(Geometria.Area.retangulo(10, 20))
