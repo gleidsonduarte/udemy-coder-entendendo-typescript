257fceb84fe65564edecc2a3a84163ab858b81e0
Gleidson Teixeira Duarte <gleidson.duarte@pravaler-153.idealinvest.local>
2020-01-20 20:35:25 -0300

    Continuação da seção 7.

--- curso-typescript/interfaces/interfaces.js
+++ curso-typescript/interfaces/interfaces.js
@@ -16,4 +16,33 @@ saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
 // saudarComOla({ nome: 'Jonas', idade: 27, altura: 1.75 })
+pessoa.saudar('Skywalker');
+// Usando Classes...
+class Cliente {
+    constructor() {
+        this.nome = '';
+        this.ultimaCompra = new Date;
+    }
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome);
+    }
+}
+const mueCliente = new Cliente();
+mueCliente.nome = 'Han';
+saudarComOla(mueCliente);
+mueCliente.saudar('Solo');
+console.log(mueCliente.ultimaCompra);
+let potencia;
+potencia = function (base, exp) {
+    let total = 1;
+    for (let i = 1; i <= exp; i++) {
+        total *= base;
+    }
+    return total;
+};
+console.log(potencia(3, 10));
+console.log(Math.pow(3, 10));
+console.log(Math.pow(3, 10));
+console.log(Array(10).fill(3).reduce((t, a) => t * a));
 //# sourceMappingURL=interfaces.js.map
--- curso-typescript/interfaces/interfaces.ts
+++ curso-typescript/interfaces/interfaces.ts
@@ -1,7 +1,14 @@
 interface Humano {
     nome: string
+
+    // Parâmetro opcional.
     idade?: number
-    [prop: string]: any
+
+    /*
+        Parâmetro obrigatório porém com nome não definido. Pode receber
+        um parâmetro com nome a, b, c... etc que não apresentará erro.
+    */
+    [prop: string]: any 
     saudar(sobrenome: string): void
 }
 
@@ -25,3 +32,42 @@ saudarComOla(pessoa)
 mudarNome(pessoa)
 saudarComOla(pessoa)
 // saudarComOla({ nome: 'Jonas', idade: 27, altura: 1.75 })
+pessoa.saudar('Skywalker')
+
+// Usando Classes...
+class Cliente implements Humano {
+    nome: string = ''
+    ultimaCompra: Date = new Date
+    saudar(sobrenome: string) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome)
+    }
+}
+
+const mueCliente = new Cliente()
+mueCliente.nome = 'Han'
+saudarComOla(mueCliente)
+mueCliente.saudar('Solo')
+console.log(mueCliente.ultimaCompra)
+
+interface FuncaoCalculo {
+    (a: number, b: number): number 
+}
+
+let potencia: FuncaoCalculo
+
+potencia = function(base: number, exp: number): number {
+    let total = 1
+    
+    for (let i = 1; i <= exp; i++) {
+        total *= base
+    }
+
+    return total
+}
+
+console.log(potencia(3, 10))
+console.log(Math.pow(3, 10))
+console.log(3 ** 10)
+console.log(Array(10).fill(3).reduce((t, a) => t * a))
+
